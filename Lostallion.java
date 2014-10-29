640cad4a9f3b048cb1c5b544e7a3a17798403242
StridentStorm <chussean@gmail.com>
2014-10-29 16:06:33 -0400

    Prototype Lostallion

---
+++ Lostallion.java
@@ -0,0 +1,10 @@
+public class Lostallion extends Enemy {
+
+    public Lostallion () {
+	HP = 150.0;
+	maxHP = 150.0; //Make this low
+	damage =100.0; //High damage
+	speed = 10; //Make this slow
+	defense = 100; //Tanky
+	multiplier = 5.0; //?? Just do whatever
+    }
