1600373e993e84902c5ab64f6a90522b01eb93ac
StridentStorm <chussean@gmail.com>
2014-10-29 16:32:47 -0400

    Door.java needs stats filled in

---
+++ Door.java
@@ -0,0 +1,18 @@
+import java.util.*
+public class Door extends Enemy {
+    //Ill leave this constructor to you Lucas
+    public Door () {
+    }
+
+    public void fall() {
+	Random rand = new Random();
+	if (rand.nextInt(100) + 1 <= accuracy){
+	    if (c.HP - (damage * multiplier - c.defense) >= 0){
+		c.HP-=( damage * multiplier - c.defense);
+		System.out.println("Door fell on you to deal" + " " + ( damage * multiplier - c.defense) + " " + "points of damage");
+	    }
+	    else{
+	    	System.out.println("Door fell on you to deal" + " " + c.HP + " " + "points of damage");
+		c.HP = 0;
+	    }
+	
