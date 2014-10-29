9dd35763e6bca6709fb106f3bb53a4ed60f7432a
LucasWeiner <LucasWeiner96@gmail.com>
2014-10-29 16:37:30 -0400

    guy is odd tabs

--- Guy.java
+++ Guy.java
@@ -31,19 +31,19 @@ public class Guy {
 	defense = 10.0;
     }
 
-   public void attack( Link c ) {
+    public void attack( Link c ) {
 	Random rand = new Random();
 	if (rand.nextInt(100) + 1 <= accuracy){
 	    if (damage * mulitplier - c.defense > 0){
 		if (c.HP - (damage * multiplier - c.defense) >= 0){
-		c.HP-=( damage * multiplier - c.defense);
-		System.out.println(this.name + " " + "dealt" + " " + ( damage * multiplier - c.defense) + " " + "points of damage to" + " " + c.name);
-	    }
-	    else{
-	    	System.out.println(this.name + " " + "dealt" + " " + c.HP + " " + "points of damage to" + " " + c.name);
-		c.HP = 0;
-	    }
+		    c.HP-=( damage * multiplier - c.defense);
+		    System.out.println(this.name + " " + "dealt" + " " + ( damage * multiplier - c.defense) + " " + "points of damage to" + " " + c.name);
 		}
+		else{
+		    System.out.println(this.name + " " + "dealt" + " " + c.HP + " " + "points of damage to" + " " + c.name);
+		    c.HP = 0;
+		}
+	    }
 	}
 	else{
 	    System.out.println(this.name + " " + "missed!");
@@ -72,26 +72,26 @@ public class Guy {
      public void doubleEdge(Link e){
 	Random rand = new Random();
 	if (rand.nextInt(100) + 1 <= accuracy - 10){
-		if (damage * multiplier * 1.5 - e.defense > 0){
-	    if (e.HP - (damage * multiplier * 1.5 - e.defense) >= 0){
-		e.HP-=( damage * multiplier * 1.5);
-		System.out.println(this.name + " " + "dealt" + " " + ( damage * multiplier * 1.5 -e.defense) + " " + "points of damage to" + " " + e.name);
-	    }
-	    else{
-	    	System.out.println(this.name + " " + "dealt" + " " + e.HP + " " + "points of damage to" + " " + e.name);
-		e.HP = 0;
-	    }
+	    if (damage * multiplier * 1.5 - e.defense > 0){
+		if (e.HP - (damage * multiplier * 1.5 - e.defense) >= 0){
+		    e.HP-=( damage * multiplier * 1.5);
+		    System.out.println(this.name + " " + "dealt" + " " + ( damage * multiplier * 1.5 -e.defense) + " " + "points of damage to" + " " + e.name);
 		}
+		else{
+		    System.out.println(this.name + " " + "dealt" + " " + e.HP + " " + "points of damage to" + " " + e.name);
+		    e.HP = 0;
+		}
+	    }
 		if (damage * multiplier / 4 - defense) > 0{
-	    HP -= (damage * multiplier / 4 - defense);
-	    System.out.println(this.name + "'s ferocious attack recoiled and did" + " " + (damage * multiplier / 4 - defense) + " " + "points of self inflicted damage (masochist)");
-		}
+			HP -= (damage * multiplier / 4 - defense);
+			System.out.println(this.name + "'s ferocious attack recoiled and did" + " " + (damage * multiplier / 4 - defense) + " " + "points of self inflicted damage (masochist)");
+		    }
 	}
 	else{
 	    System.out.println(this.name + " " + "missed bad");
 	}
 	
-    }
+     }
     
     public void strongAttack(Link e){
 	Random rand = new Random();
